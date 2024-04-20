b47234dceb1cafd45af9dadaeceef503736f6688
Willians <prwillians.costa@gmail.com>
2024-04-20 05:09:31 -0300

    Adiciona Arquivos da Pasta Lista

---
+++ Documentando-em-Codigos/Listas/PrincipaisMetodos.md
@@ -0,0 +1,12 @@
+Count:        retorna o número de elementos na lista.
+Clear:        remove todos os elementos da lista.
+Add:          adiciona um elemento à lista.
+RemoveAt:     remove um elemento da lista em uma determinada posição.
+Remove:       remove o primeiro elemento da lista que corresponde a um determinado valor.
+Contains:     verifica se um determinado valor está presente na lista.
+IndexOf:      retorna a posição do primeiro elemento na lista que corresponde a um determinado valor.
+LastIndexOf:  retorna a posição do último elemento na lista que corresponde a um determinado valor.
+ToArray:      converte a lista em um array.
+ToList:       converte a lista em uma lista genérica.
+Reverse:      inverte a ordem dos elementos na lista.
+Sort:         ordena os elementos na lista.
---
+++ Documentando-em-Codigos/Listas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numeros = new List<int> { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("A lista tem " + numeros.Count + " elementos.");
+        Console.WriteLine("O primeiro elemento da lista é " + numeros[0] + ".");
+        Console.WriteLine("O último elemento da lista é " + numeros[numeros.Count - 1] + ".");
+
+        numeros.Add(6);
+        numeros.RemoveAt(0);
+
+        Console.WriteLine("Após adicionar um elemento e remover o primeiro, a lista tem " + numeros.Count + " elementos.");
+        Console.WriteLine("O primeiro elemento da lista é " + numeros[0] + ".");
+        Console.WriteLine("O último elemento da lista é " + numeros[numeros.Count - 1] + ".");
+
+        if (numeros.Contains(3))
+        {
+            Console.WriteLine("O valor 3 está na lista.");
+        }
+
+        int posicao = numeros.IndexOf(4);
+        Console.WriteLine("O valor 4 está na posição " + posicao + " da lista.");
+
+        numeros.Reverse();
+        Console.WriteLine("Após inverter a lista, o primeiro elemento é " + numeros[0] + ".");
+
+        numeros.Sort();
+        Console.WriteLine("Após ordenar a lista, o primeiro elemento é " + numeros[0] + ".");
+    }
+}
